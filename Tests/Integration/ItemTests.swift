a1bbe54776d069141089769eff580036139828ce
Claus Höfele <claus.hoefele@here.com>
2016-09-12 20:29:45 +0200

    Trying out router invocation

--- Tests/Integration/ItemTests.swift
+++ Tests/Integration/ItemTests.swift
@@ -1,5 +1,6 @@
 @testable import SwiftServerLibrary
 import Kitura
+import MongoKitten
 import XCTest
 
 class ItemTests: XCTestCase {
@@ -10,4 +11,23 @@ class ItemTests: XCTestCase {
         
         XCTAssertEqual(40, 40)
     }
+
+//    func testRouter() {
+//        guard let mongoServer = try? Server("mongodb://localhost", automatically: false) else { XCTAssert(false); return }
+//        let router = Router()
+//
+//        let itemEndpoint = CRUDMongoEndpoint(collection: mongoServer["db"]["items"],
+//                                             generateDocument: ItemEndpoint.generateDocument,
+//                                             generateJsonDictionary: ItemEndpoint.generateJsonDictionary)
+//        let itemHandler = CRUDMongoHandler(endpoint: itemEndpoint)
+//        router.all("/items", handler: itemHandler.handleItems)
+//        router.all("/items/:id", handler: itemHandler.handleItem)
+//
+//
+//        let e = expectation(description: "test")
+//        router.get("/items") { request, response, next in
+//            response.send("pong")
+//            next()
+//        }
+//    }
 }
