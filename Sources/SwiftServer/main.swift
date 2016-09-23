676d82c0412807d2b9c15696de16fc55c50c7be0
Mircea Gindulescu <mircea.gindulescu@here.com>
2016-09-23 16:55:27 +0200

    Add request for last completed, stable and failed builds

--- Sources/SwiftServer/main.swift
+++ Sources/SwiftServer/main.swift
@@ -1,9 +1,12 @@
 import Kitura
+import KituraNet
 import HeliumLogger
 import LoggerAPI
 import MongoKitten
 import Environment
 import SwiftServerLibrary
+import SwiftyJSON
+import Foundation
 
 HeliumLogger.use()
 
@@ -11,28 +14,44 @@ for e in Env.all() {
     Log.info("\(e.key) = \(e.value)")
 }
 
-let dbUrl = Env["DB_URL"] ?? "mongodb://localhost"
-do {
-    let mongoServer = try Server(dbUrl)
-    try mongoServer.connect()
-    Log.info("Connected to Mongo DB \(dbUrl)")
+//let dbUrl = Env["DB_URL"] ?? "mongodb://localhost"
+//do {
+//    let mongoServer = try Server(dbUrl)
+//    try mongoServer.connect()
+//    Log.info("Connected to Mongo DB \(dbUrl)")
 
     let router = Router()
 
-    let itemEndpoint = CRUDMongoEndpoint(collection: mongoServer["test"]["items"],
-                                         generateDocument: ItemEndpoint.generateDocument,
-                                         generateJsonDictionary: ItemEndpoint.generateJsonDictionary)
-    let itemHandler = CRUDMongoHandler(endpoint: itemEndpoint)
-    router.all("/items", handler: itemHandler.handleItems)
-    router.all("/items/:id", handler: itemHandler.handleItem)
+//    let itemEndpoint = CRUDMongoEndpoint(collection: mongoServer["db"]["items"],
+//                                         generateDocument: ItemEndpoint.generateDocument,
+//                                         generateJsonDictionary: ItemEndpoint.generateJsonDictionary)
+//    let itemHandler = CRUDMongoHandler(endpoint: itemEndpoint)
+//    router.all("/items", handler: itemHandler.handleItems)
+//    router.all("/items/:id", handler: itemHandler.handleItem)
 
     router.get("/ping") { request, response, next in
         response.send("pong")
         next()
     }
 
+    router.get("/jenkins") {request, response, next in
+        JenkinsService.fetchAllBuilds({ (jenkinsResponse) in
+            switch(jenkinsResponse) {
+            case .error(let errorCode):
+                response.status(.internalServerError)
+            case .success(let builds):
+                let displayString = "\(builds.completed) - \(builds.stable) - \(builds.failed)"
+                response.send(displayString)
+            }
+
+            next()
+        })
+    }
+
+
+
     Kitura.addHTTPServer(onPort: 8090, with: router)
     Kitura.run()
-} catch {
-    Log.error("Cannot connect to Mongo DB \(dbUrl)")
-}
+//} catch {
+//    Log.error("Cannot connect to Mongo DB \(dbUrl)")
+//}
--- Tests/Integration/ItemTests.swift
+++ Tests/Integration/ItemTests.swift
@@ -1,72 +1,98 @@
 @testable import SwiftServerLibrary
-import Kitura
-import MongoKitten
+//import Kitura
+//import MongoKitten
 import XCTest
-
+import SwiftyJSON
+//
 class ItemTests: XCTestCase {
-    func testGetRequestStatusCode() {
-        let e = expectation(description: "test")
-        e.fulfill()
-        waitForExpectations(timeout: 1)
-        
-        XCTAssertEqual(40, 40)
-    }
-
-    func testGenerateJsonDictionary() {
-        let id = ObjectId()
-        let name = "name"
-        var document = Document()
-        document["_id"] = .objectId(id)
-        document["name"] = .string(name)
-
-        let jsonDictionary = ItemEndpoint.generateJsonDictionary(document: document)
-        XCTAssertEqual(jsonDictionary["id"] as? String, id.hexString)
-        XCTAssertEqual(jsonDictionary["name"] as? String, name)
-    }
-
-//    func testRouter() {
-//        guard let mongoServer = try? Server("mongodb://localhost", automatically: false) else { XCTAssert(false); return }
-//        let router = Router()
-//
-//        let itemEndpoint = CRUDMongoEndpoint(collection: mongoServer["db"]["items"],
-//                                             generateDocument: ItemEndpoint.generateDocument,
-//                                             generateJsonDictionary: ItemEndpoint.generateJsonDictionary)
-//        let itemHandler = CRUDMongoHandler(endpoint: itemEndpoint)
-//        router.all("/items", handler: itemHandler.handleItems)
-//        router.all("/items/:id", handler: itemHandler.handleItem)
-//
-//
+//    func testGetRequestStatusCode() {
 //        let e = expectation(description: "test")
-//        router.get("/items") { request, response, next in
-//            response.send("pong")
-//            next()
-//        }
+//        e.fulfill()
+//        waitForExpectations(timeout: 1)
+//        
+//        XCTAssertEqual(40, 40)
 //    }
+//
+//    func testGenerateJsonDictionary() {
+//        let id = ObjectId()
+//        let name = "name"
+//        var document = Document()
+//        document["_id"] = .objectId(id)
+//        document["name"] = .string(name)
+//
+//        let jsonDictionary = ItemEndpoint.generateJsonDictionary(document: document)
+//        XCTAssertEqual(jsonDictionary["id"] as? String, id.hexString)
+//        XCTAssertEqual(jsonDictionary["name"] as? String, name)
+//    }
+//    
+    func testJenkinsResponseWithInsufficientParams() {
+        let json: JSON = [
+            "lastCompleteBuild": ["number": 10],
+            "lastSuccessBuild": ["number": 2],
+            ]
+        
+        let parseResult = JenkinsService.parseAllBuildsResult(json)
+        
+        XCTAssertNil(parseResult)
+    }
 
-//    func testKitura() {
-//        // Set up router for this test
-//        let router = Router()
+    func testJenkinsResponseWithSimpleValues() {
+        let json: JSON = [
+            "lastCompletedBuild": ["number": 10],
+            "lastStableBuild": ["number": 2],
+            "lastFailedBuild": ["number": 10],
+            ]
+
+        let parseResult = JenkinsService.parseAllBuildsResult(json)
+
+        XCTAssertEqual(parseResult?.completed, 10)
+        XCTAssertEqual(parseResult?.stable, 2)
+        XCTAssertEqual(parseResult?.failed, 10)
+    }
 //
-//        router.get("/zxcv/:p1") { request, _, next in
-//            let parameter = request.parameters["p1"]
-//            XCTAssertNotNil(parameter, "URL parameter p1 was nil")
-//            next()
-//        }
-//        router.get("/zxcv/ploni") { request, _, next in
-//            let parameter = request.parameters["p1"]
-//            XCTAssertNil(parameter, "URL parameter p1 was not nil, it's value was \(parameter!)")
-//            next()
-//        }
-//        router.all() { _, response, next in
-//            response.status(.OK).send("OK")
-//            next()
-//        }
+////    func testRouter() {
+////        guard let mongoServer = try? Server("mongodb://localhost", automatically: false) else { XCTAssert(false); return }
+////        let router = Router()
+////
+////        let itemEndpoint = CRUDMongoEndpoint(collection: mongoServer["db"]["items"],
+////                                             generateDocument: ItemEndpoint.generateDocument,
+////                                             generateJsonDictionary: ItemEndpoint.generateJsonDictionary)
+////        let itemHandler = CRUDMongoHandler(endpoint: itemEndpoint)
+////        router.all("/items", handler: itemHandler.handleItems)
+////        router.all("/items/:id", handler: itemHandler.handleItem)
+////
+////
+////        let e = expectation(description: "test")
+////        router.get("/items") { request, response, next in
+////            response.send("pong")
+////            next()
+////        }
+////    }
 //
-//        performServerTest(router) { expectation in
-//            self.performRequest("get", path: "/zxcv/ploni", callback: { response in
-//                XCTAssertNotNil(response, "ERROR!!! ClientRequest response object was nil")
-//                expectation.fulfill()
-//            })
-//        }
-//    }
+////    func testKitura() {
+////        // Set up router for this test
+////        let router = Router()
+////
+////        router.get("/zxcv/:p1") { request, _, next in
+////            let parameter = request.parameters["p1"]
+////            XCTAssertNotNil(parameter, "URL parameter p1 was nil")
+////            next()
+////        }
+////        router.get("/zxcv/ploni") { request, _, next in
+////            let parameter = request.parameters["p1"]
+////            XCTAssertNil(parameter, "URL parameter p1 was not nil, it's value was \(parameter!)")
+////            next()
+////        }
+////        router.all() { _, response, next in
+////            response.status(.OK).send("OK")
+////            next()
+////        }
+////
+////        performServerTest(router) { expectation in
+////            self.performRequest("get", path: "/zxcv/ploni", callback: { response in
+////                XCTAssertNotNil(response, "ERROR!!! ClientRequest response object was nil")
+////                expectation.fulfill()
+////            })
+////        }
+////    }
 }
